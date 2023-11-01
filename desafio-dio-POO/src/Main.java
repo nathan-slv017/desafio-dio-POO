45517803c62ac7c52e93b392768e91cb0d7a44a0
Nathan da Silva Barrozo <email.do.natann@gmail.com>
2023-11-01 20:17:24 -0300

    1/11/2023 - 3

--- desafio-dio-POO/src/Main.java
+++ desafio-dio-POO/src/Main.java
@@ -1,5 +1,23 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("POO");
+        curso1.setDescricao("Programação orientada a objeto");
+        curso1.setCargaHoraria(160);
+
+        System.out.println(curso1);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria De Java");
+        mentoria1.setDescricao("Descrição mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
     }
 }
